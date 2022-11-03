8c6f3a173f72f0f887229087f283d45c7552b3fe
unknown <fernandoshaggy562@gmail.com>
2022-11-02 18:55:52 -0600

    modales

--- resources/views/Materiales/materiales.blade.php
+++ resources/views/Materiales/materiales.blade.php
@@ -54,7 +54,7 @@
         </thead>
         <tbody>
 @foreach ($materiales as $material)
-<tr class="marca">
+<tr class="marca" onclick="pasar_id({{$material->id}});">
     <td style="text-align: center;">{{$material->Nombre}}</td>
     <td style="text-align: center;">{{$material->Costo}}</td>
     <td style="text-align: center;">{{$material->Unidades}}</td>
@@ -115,6 +115,70 @@
     </div>
   </div>
 
+  <!--menu de opciones de la tabla-->
+  <div id="menu_opciones" class="visible_off " style=" padding: 25px; background-color: #66697569;">
+
+    <button type="button" class="close" style="margin-right: -17px; margin-top: -20px;" onclick="cerrar_menu();">
+       <i class="fas fa-times fa-xs"></i>
+    </button>
+  <button class="btn btn-warning form-control" style="margin-bottom: 10px; font-weight: bold;" data-toggle="modal" data-target="#eliminar_mate">
+    Eliminar
+  </button>
+  <br>
+  <button type="button" class="btn btn-info form-control" style="margin-bottom: 10px; font-weight: bold;" data-toggle="modal" data-target="#editar_mate">
+    Editar
+  </button>
+
+</div>
+
+<!-- modal de eliminar material-->
+<div class="modal fade" id="eliminar_mate" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Eliminar Material</h5>
+        </div>
+        <form action="" method="POST">
+            @csrf
+            @method('DELETE')
+            <div class="modal-body">
+              <label style="font-family: cursive; font-size: 25px; display:flex; justify-content: center;align-items: center; height: 100%;">¿Quieres Eliminar el Material?</label><br><br>
+                <div style="text-align: center;">
+                    <label style="font-family: cursive; font-size: 25px;" id="labeleliminar"></label>
+                     </div>
+            </div>
+            <div class="modal-footer">
+                <input type="hidden" name="id_mate" id="id_mate">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                <button class="btn btn-danger" >Eliminar</button>
+            </div>
+        </form>
+    </div>
+  </div>
+</div>
+
+<!-- modal de editar material-->
+<div class="modal fade" id="editar_mate" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Editar Material</h5>
+        </div>
+        <form action="" method="POST">
+            @csrf
+            <div class="modal-body">
+
+
+            </div>
+            <div class="modal-footer">
+                <input type="hidden" name="id_mate2" id="id_mate2">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                <button class="btn btn-success" >Editar</button>
+            </div>
+        </form>
+    </div>
+  </div>
+</div>
 
     </div>
 </div>
@@ -144,6 +208,19 @@ input[type=number] { -moz-appearance:textfield; }
         background: #797d8b80;
         transition: 1s;
     }
+    .visible_on{
+        display: block;
+        position: fixed;
+        background: white;
+        border-radius: 15px;
+        width: auto;
+    }
+    .visible_off{
+        display: none;
+    }
+    .paginate_button{
+    position:sticky;
+}
 </style>
 
 @stop
@@ -185,6 +262,29 @@ document.getElementById("costo_uso").value=costouso.toFixed(2);
 $(".alert").fadeTo(3000, 500).slideUp(500, function(){
     $(".alert").slideUp(500);
 });
+
+//cuadro de opciones
+var id_material=null;
+    function pasar_id($id_tr) {
+
+        id_user=$id_tr;
+        var coordenadas_y=event.clientY; //odtenemos el valor de la posicion del boton
+        var coordenadas_x=event.clientX; //odtenemos el valor de la posicion del boton
+        menu_opciones.style.top=coordenadas_y-50+"px";
+        menu_opciones.style.left=coordenadas_x-50+"px";
+        menu_opciones.classList.add("visible_on");
+        menu_opciones.classList.remove("visible_off");
+      //alert($id_tr);
+    }
+    menu_opciones.addEventListener("mouseleave",function(){
+          menu_opciones.classList.remove("visible_on");
+          menu_opciones.classList.add("visible_off");
+    });
+    function cerrar_menu(){
+        menu_opciones.classList.remove("visible_on");
+        menu_opciones.classList.add("visible_off");
+    }
+
 </script>
 
 @stop
